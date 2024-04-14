2a6447a81b727f92f17b8492549e98a80403e74b
AitanaGS <aitana.graebs@gmail.com>
2024-04-14 10:38:06 +0200

    Update header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,7 +3,7 @@
     <div>
         <p>
             In many countries, people face threats to their safety and well-being because of who 
-            they love or how they identify. Each year, the Spartacus International Gay Guide 
+            they are or who they love. Each year, the Spartacus International Gay Guide 
             releases the Gay Travel Index (GTI). The index helps travelers by keeping track 
             of the rights of local LGBTQIA+ communities in each country, 
             ensuring they can travel safely. It also provides insight into the lives of people 
